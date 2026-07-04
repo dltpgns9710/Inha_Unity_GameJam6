f0e70a2a3e791fa6145a710fc4c5438481aac2ac
ahqja3648 <ahqja3648@naver.com>
2026-07-04 15:10:44 +0900

    fix : 플레이어 인풋 시스템 오류 수정

--- Assets/Project/Scripts/Camera/MainCameraComtroller.cs
+++ Assets/Project/Scripts/Camera/MainCameraComtroller.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
-public class MainCameraComtroller : MonoBehaviour
+namespace JUNBEOM.Camera
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class MainCameraComtroller : MonoBehaviour
     {
-        
-    }
+        #region Serialized Fields
+        [SerializeField] Transform player;
+        [SerializeField] float smoothing = 0.2f;
+        [SerializeField] Vector2 minCameraBoundary;
+        [SerializeField] Vector2 maxCameraBoundary;
+        #endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        #region Unity Lifecycle
+        private void FixedUpdate()
+        {
+            Vector3 targetPos = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+
+            targetPos.x = Mathf.Clamp(targetPos.x, minCameraBoundary.x, maxCameraBoundary.x);
+            targetPos.y = Mathf.Clamp(targetPos.y, minCameraBoundary.y, maxCameraBoundary.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
+        }
+        #endregion
     }
 }
