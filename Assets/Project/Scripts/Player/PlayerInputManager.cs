f037cfc15fa115ba4f87afc56fca1998339b1918
ahqja3648 <ahqja3648@naver.com>
2026-07-11 14:08:50 +0900

    fix : PlayerInputActions 수정, Player prefab 수정

--- Assets/Project/Scripts/Player/PlayerInputManager.cs
+++ Assets/Project/Scripts/Player/PlayerInputManager.cs
@@ -8,7 +8,7 @@ namespace JUNBEOM.Player
     {
         #region Private Fields
 
-        private PlayerInputActions _inputActions;
+        private PlayerInput _playerInput;
 
         #endregion
 
@@ -33,41 +33,36 @@ namespace JUNBEOM.Player
 
         private void Awake()
         {
-            _inputActions = new PlayerInputActions();
+            _playerInput = GetComponent<PlayerInput>();
 
-            // Player Action Map 구독
-            _inputActions.Player.Move.performed += OnMove;
-            _inputActions.Player.Move.canceled += OnMove;
+            _playerInput.actions["Move"].performed += OnMove;
+            _playerInput.actions["Move"].canceled += OnMove;
 
-            _inputActions.Player.Run.performed += OnRun;
-            _inputActions.Player.Run.canceled += OnRun;
+            _playerInput.actions["Run"].performed += OnRun;
+            _playerInput.actions["Run"].canceled += OnRun;
 
-            _inputActions.Player.Jump.started += OnJump;
-            _inputActions.Player.EquipFlashlight.started += OnEquipFlashlight;
-            _inputActions.Player.ToggleFlashlight.started += OnToggleFlashlight;
+            _playerInput.actions["Jump"].started += OnJump;
+            _playerInput.actions["EquipFlashlight"].started += OnEquipFlashlight;
+            _playerInput.actions["ToggleFlashlight"].started += OnToggleFlashlight;
 
-            _inputActions.Player.Interact.started += OnInteract;
+            _playerInput.actions["Interact"].started += OnInteract;
 
-            _inputActions.Player.ReturnCamera.started += OnReturnCamera;
+            _playerInput.actions["ReturnCamera"].started += OnReturnCamera;
 
-            _inputActions.Player.SelectCompanionCommandZ.started += OnSelectCompanionCommandZ;
-            _inputActions.Player.SelectCompanionCommandX.started += OnSelectCompanionCommandX;
-            _inputActions.Player.ConfirmCompanionCommand.started += OnConfirmCompanionCommand;
-            _inputActions.Player.CancelCompanionCommand.started += OnConcelCompanionCommand;
-
-            // UI Action Map 구독
-            _inputActions.UI.ToggleSettings.started += OnToggleSettings;
+            _playerInput.actions["SelectCompanionCommandZ"].started += OnSelectCompanionCommandZ;
+            _playerInput.actions["SelectCompanionCommandX"].started += OnSelectCompanionCommandX;
+            _playerInput.actions["ConfirmCompanionCommand"].started += OnConfirmCompanionCommand;
+            _playerInput.actions["CancelCompanionCommand"].started += OnCancelCompanionCommand; 
         }
 
         private void OnEnable()
         {
             EnablePlayerInput();
-            //_inputActions.UI.Enable(); // UI 액션맵은 항상 활성화
         }
 
         private void OnDisable()
         {
-            _inputActions.Disable();
+            DisablePlayerInput(); 
         }
 
         #endregion
@@ -76,13 +71,13 @@ namespace JUNBEOM.Player
 
         public void EnablePlayerInput()
         {
-            _inputActions.Player.Enable();
+            _playerInput.actions.Enable();
         }
 
         public void DisablePlayerInput()
         {
-            _inputActions.Player.Disable();
-            OnMoveEvent?.Invoke(0f); // 입력 차단 시 이동 취소 보정
+            _playerInput.actions.Disable();
+            OnMoveEvent?.Invoke(0f);
             OnRunEvent?.Invoke(false);
         }
 
@@ -137,7 +132,7 @@ namespace JUNBEOM.Player
         {
             OnConfirmCompanionCommandEvent?.Invoke();
         }
-        private void OnConcelCompanionCommand(InputAction.CallbackContext context)
+        private void OnCancelCompanionCommand(InputAction.CallbackContext context)
         {
             OnCancelCompanionCommandEvent?.Invoke();
         }
