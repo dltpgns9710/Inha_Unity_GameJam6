f866d4c592b7130c042906847aad79ecb6b50c0c
DESKTOP-B6PNU2R\LSH <dltpgns9710@gmail.com>
2026-07-13 13:40:20 +0900

    feat : StoryScene contoller 추가 - 이세훈 - stroyType 에 따른 대화창 표시 추가 - storytype 에 따른 bgm 추가

---
+++ Assets/Project/Scripts/UI/SceneController.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using SEHOON.GameSystem;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SEHOON.UI
+{
+    public class SceneController : MonoBehaviour
+    {
+        [Serializable]
+        public struct StoryTypeEntry
+        {
+            public EStoryType StoryType;
+            public GameObject DialogueWindow;
+            public string SceneName;
+            public AudioClip AudioClip;
+        }
+
+        #region Serialized Fields
+        [Header("Story Type Entries")]
+        [SerializeField] private List<StoryTypeEntry> _storyTypeEntries = new List<StoryTypeEntry>();
+
+        [Header("Fallback")]
+        [SerializeField] private string _fallbackScene;
+        #endregion
+
+        #region Unity Lifecycle
+        private void Start()
+        {
+            EStoryType currentStoryType = DataManager.Instance.StoryType;
+
+            foreach (StoryTypeEntry entry in _storyTypeEntries)
+            {
+                entry.DialogueWindow?.SetActive(entry.StoryType == currentStoryType);
+            }
+        }
+
+        private void OnEnable()
+        {
+            AudioClip clip = GetCurrentAudioClip();
+            if (clip == null) return;
+
+            SoundManager.Instance.PlayBgm(clip);
+        }
+
+        private void OnDisable()
+        {
+            SoundManager.Instance.StopBgm();
+        }
+        #endregion
+
+        #region Public Methods
+        public void ChangeScene()
+        {
+            SceneManager.LoadScene(GetSceneToLoad());
+        }
+        #endregion
+
+        #region Private Methods
+        private string GetSceneToLoad()
+        {
+            EStoryType currentStoryType = DataManager.Instance.StoryType;
+
+            foreach (StoryTypeEntry entry in _storyTypeEntries)
+            {
+                if (entry.StoryType == currentStoryType)
+                {
+                    return entry.SceneName;
+                }
+            }
+
+            return _fallbackScene;
+        }
+
+        private AudioClip GetCurrentAudioClip()
+        {
+            EStoryType currentStoryType = DataManager.Instance.StoryType;
+
+            foreach (StoryTypeEntry entry in _storyTypeEntries)
+            {
+                if (entry.StoryType == currentStoryType)
+                {
+                    return entry.AudioClip;
+                }
+            }
+
+            return null;
+        }
+        #endregion
+    }
+}
