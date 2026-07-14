d2944daccfdcba8d3e07dce891eb8be5f0ad8e2a
ahqja3648 <ahqja3648@naver.com>
2026-07-14 15:03:22 +0900

    feat : anomaly test

---
+++ Assets/Project/Scripts/Anomaly/AnomalyTest.cs
@@ -0,0 +1,15 @@
+using SEHOON.GameSystem;
+using UnityEngine;
+
+public class AnomalyTest : AnomalyBase
+{
+    public override void Apply()
+    {
+
+    }
+
+    public override void Remove()
+    {
+            
+    }
+}
