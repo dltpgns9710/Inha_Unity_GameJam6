bd0f20edbf1e877116c5dfab60598761be741828
DESKTOP-B6PNU2R\LSH <dltpgns9710@gmail.com>
2026-07-14 20:23:25 +0900

    feat : 이상현상 개발 중 사용하기 위한 디버그 시스템 추가 - 이세훈

--- Assets/Project/Scripts/Manager/AnomalyManager.cs
+++ Assets/Project/Scripts/Manager/AnomalyManager.cs
@@ -8,16 +8,15 @@ namespace SEHOON.GameSystem
         [SerializeField] private List<AnomalyBase> _anomalies;
 
         private AnomalyBase _selectedAnomaly = null;
-        private bool _isAnomalyApply = false;
 
-        public bool IsAnomalyApply => _isAnomalyApply;
 
         private void Start()
         {
-            _isAnomalyApply = false;
+            DataManager.Instance.IsAnomalyApply = false;
             SelectAnomaly();
             if (_selectedAnomaly != null)
             {
+                DataManager.Instance.IsAnomalyApply = true;
                 _selectedAnomaly.Apply();
             }
         }
@@ -27,11 +26,13 @@ namespace SEHOON.GameSystem
             if (_selectedAnomaly != null)
             {
                 _selectedAnomaly.Remove();
+                DataManager.Instance.IsAnomalyApply = false;
             }
         }
 
         private void SelectAnomaly()
         {
+            _selectedAnomaly = _anomalies[0];
             /*
             if (Random.Range(0, 10) > 1)
             {
@@ -41,5 +42,29 @@ namespace SEHOON.GameSystem
             }
             */
         }
+
+#if UNITY_EDITOR
+        public int CurrentAnomalyIndex => _selectedAnomaly != null ? _anomalies.IndexOf(_selectedAnomaly) : -1;
+
+        public void DebugApplyAnomaly(int index)
+        {
+            if (index < 0 || index >= _anomalies.Count) return;
+
+            if (_selectedAnomaly != null) _selectedAnomaly.Remove();
+
+            _selectedAnomaly = _anomalies[index];
+            _selectedAnomaly.Apply();
+            DataManager.Instance.IsAnomalyApply = true;
+        }
+
+        public void DebugRemoveAnomaly()
+        {
+            if (_selectedAnomaly == null) return;
+
+            _selectedAnomaly.Remove();
+            _selectedAnomaly = null;
+            DataManager.Instance.IsAnomalyApply = false;
+        }
+#endif
     }
 }
