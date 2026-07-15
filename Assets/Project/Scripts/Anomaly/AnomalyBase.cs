7ec0c1e801894b713847908d3ce45085d0394bec
DESKTOP-K9H7FNG\inha <dltpgns9710@gmail.com>
2026-07-15 15:12:20 +0900

    feat : 이상현상 감지를 위한 데이터 추가 - 이세훈

--- Assets/Project/Scripts/Anomaly/AnomalyBase.cs
+++ Assets/Project/Scripts/Anomaly/AnomalyBase.cs
@@ -2,12 +2,28 @@ using UnityEngine;
 
 namespace SEHOON.GameSystem
 {
+    public enum EAnomalyType
+    {
+        Local,
+        Global
+    };
+    
+    [System.Serializable]
+    public struct SDetectData
+    {
+        [SerializeField] EAnomalyType Type;
+        [SerializeField] Vector3 AnomalyPos;
+        [SerializeField, Range(0, 60)]float DetectRange;
+    };
+    
     public abstract class AnomalyBase : MonoBehaviour
     {
+        [SerializeField] private SDetectData _detectData;
         [SerializeField] private float _weight = 1f;
 
-        public float Weight => _weight;
-
+        public float Weight { get; }
+        public SDetectData DetectData { get; }
+        
         public abstract void Apply();
         public abstract void Remove();
     }
