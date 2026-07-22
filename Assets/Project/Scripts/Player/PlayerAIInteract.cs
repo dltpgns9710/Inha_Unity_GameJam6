741df1b16fbfdb82a18793de57e9a9c79f056d7d
ahqja3648 <ahqja3648@naver.com>
2026-07-22 16:50:16 +0900

    fix : player fix

--- Assets/Project/Scripts/Player/PlayerAIInteract.cs
+++ Assets/Project/Scripts/Player/PlayerAIInteract.cs
@@ -2,17 +2,19 @@ using System;
 using TAEWOOK.Helper.Core;
 using UnityEngine;
 using JUNBEOM.Player;
-    public class PlayerAIInteract : MonoBehaviour
-    {
-        [Header("References")]
-        [SerializeField] private PlayerInputManager _inputManager;
-        [SerializeField] private HelperCommandBroadcaster _HelperInteract;
-        public Texture2D _findCursor;
-        public Texture2D _normalCursor;
+public class PlayerAIInteract : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private PlayerInputManager _inputManager;
+    [SerializeField] private HelperCommandBroadcaster _HelperInteract;
+    [SerializeField] private HelperControllar _HelperController;
+    public Texture2D _findCursor;
+    public Texture2D _normalCursor;
+    public Texture2D _cancleCursor;
 
     #region Private Fields
-        private bool _canDetect;
-        private Vector2 hotSpot = Vector2.zero;
+    private bool _canDetect;
+    private Vector2 hotSpot = Vector2.zero;
     #endregion
     private void Start()
     {
@@ -20,46 +22,56 @@ using JUNBEOM.Player;
     }
 
     private void OnEnable()
+    {
+        _inputManager.OnRequestDetectAnomalyEvent += HandleRequestDetectAnomaly;
+        _inputManager.OnRequestWaitEvent += HandleRequestWait;
+        _inputManager.OnConfirmEvent += HandleConfirm;
+        _inputManager.OnCancelEvent += HandleCancel;
+    }
+
+    private void OnDisable()
+    {
+        _inputManager.OnRequestDetectAnomalyEvent -= HandleRequestDetectAnomaly;
+        _inputManager.OnRequestWaitEvent -= HandleRequestWait;
+        _inputManager.OnConfirmEvent -= HandleConfirm;
+        _inputManager.OnCancelEvent -= HandleCancel;
+    }
+
+
+    private void HandleRequestDetectAnomaly()
+    {
+        if (_HelperController.HasDetectChance)
         {
-            _inputManager.OnRequestDetectAnomalyEvent += HandleRequestDetectAnomaly;
-            _inputManager.OnRequestWaitEvent += HandleRequestWait;
-            _inputManager.OnConfirmEvent += HandleConfirm;
-            _inputManager.OnCancelEvent += HandleCancel;
+            Cursor.SetCursor(_cancleCursor, hotSpot, CursorMode.Auto);
+            _canDetect = false;
         }
-
-        private void OnDisable()
+        else
         {
-            _inputManager.OnRequestDetectAnomalyEvent -= HandleRequestDetectAnomaly;
-            _inputManager.OnRequestWaitEvent -= HandleRequestWait;
-            _inputManager.OnConfirmEvent -= HandleConfirm;
-            _inputManager.OnCancelEvent -= HandleCancel;
-        }
-
-
-        private void HandleRequestDetectAnomaly()
-        {            
             Cursor.SetCursor(_findCursor, hotSpot, CursorMode.Auto);
             _canDetect = true;
         }
-
-        private void HandleRequestWait()
-        {
-            _HelperInteract.RequestWait();            
-        }
-
-        private void HandleConfirm()
-        {
-            if (!_canDetect)
-                return;
-            Vector2 searchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            _HelperInteract.RequestDetectAnomaly(searchPosition);            
-            Cursor.SetCursor(_normalCursor, hotSpot, CursorMode.Auto);
-            _canDetect = false;
-        }
-
-        private void HandleCancel()
-        {            
-            Cursor.SetCursor(_normalCursor, hotSpot, CursorMode.Auto);
-        }
     }
 
+    private void HandleRequestWait()
+    {
+        _HelperInteract.RequestWait();
+    }
+
+    private void HandleConfirm()
+    {
+        if (!_canDetect)
+        {
+            Cursor.SetCursor(_normalCursor, hotSpot, CursorMode.Auto);
+            return;
+        }
+        Vector2 searchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        _HelperInteract.RequestDetectAnomaly(searchPosition);
+        Cursor.SetCursor(_normalCursor, hotSpot, CursorMode.Auto);
+        _canDetect = false;
+    }
+
+    private void HandleCancel()
+    {
+        Cursor.SetCursor(_normalCursor, hotSpot, CursorMode.Auto);
+    }
+}
